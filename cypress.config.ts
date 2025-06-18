1174ef88334a03b37e83a7792178c1efb49c0a85
john <dinura@qbitum.com>
2025-06-18 08:50:24 +0530

    first commit

---
+++ cypress.config.ts
@@ -0,0 +1,67 @@
+import { defineConfig } from "cypress";
+import { configureAllureAdapterPlugins } from '@mmisty/cypress-allure-adapter/plugins';
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  retries: {
+    runMode: 0,
+    openMode: 0,
+  },
+  env: {
+    grepOmitFiltered: true,
+    grepFilterSpecs: true,
+    allure: true,
+    allureCleanResults: true,
+    allureSkipCommands: 'wrap,screenshot,wait',
+    allureResults: 'allure-results',
+    allureAttachRequests: true,
+  },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'spec, mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/report/mochawesome-report',
+      reportFilename: "[datetime]-[name]-report",
+      timestamp: "isoUtcDateTime",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
+  },
+
+  e2e: {
+    fixturesFolder: 'cypress/fixtures',
+    defaultCommandTimeout: 3000,
+    video: false,
+
+    setupNodeEvents(on, config) {
+      // Add Cypress Grep Plugin
+      require('@cypress/grep/src/plugin')(config);
+
+      // Add Cypress Terminal Report Plugin
+      require('cypress-terminal-report/src/installLogsPrinter')(on);
+
+      // Add Allure Adapter Plugin
+      const reporter = configureAllureAdapterPlugins(on, config);
+
+      on('before:run', (details) => {
+        reporter?.writeEnvironmentInfo({
+          info: {
+            os: details.system.osName,
+            osVersion: details.system.osVersion,
+            browser: `${details.browser?.displayName} ${details.browser?.version}`,
+            ...config.env,
+          },
+        });
+
+        reporter?.writeCategoriesDefinitions({ categories: './allure-error-categories.json' });
+      });
+
+      // Task to get the latest file in a directory
+      
+
+
+      return config;
+    },
+  },
+});
